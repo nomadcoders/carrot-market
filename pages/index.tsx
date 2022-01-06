565df541e2bfd2f4885116d93191a9d094ccce26
Nicolás Serrano Arévalo <itnico.las.me@gmail.com>
2022-01-06 15:14:11 +0900

    2.0 Introduction

--- pages/index.tsx
+++ pages/index.tsx
@@ -2,7 +2,7 @@ import type { NextPage } from "next";
 
 const Home: NextPage = () => {
   return (
-    <div className="bg-red-500">
+    <div className="bg-red-500 text-center flex">
       <h1 className="text-black">it works</h1>
     </div>
   );
