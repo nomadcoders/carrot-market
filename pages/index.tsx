9d57d87c5c2bc35a06b95f6f6d2b29d78c037ebc
Nicolás Serrano Arévalo <itnico.las.me@gmail.com>
2022-01-06 19:56:21 +0900

    2.4 Modifiers

--- pages/index.tsx
+++ pages/index.tsx
@@ -17,13 +17,15 @@ const Home: NextPage = () => {
           <span>Total</span>
           <span className="font-semibold">$10</span>
         </div>
-        <div
+        <button
           className="mt-5 bg-blue-500 text-white p-3
-          text-center rounded-xl w-3/4 mx-auto
+          text-center rounded-xl w-3/4 mx-auto 
+          hover:bg-teal-500 hover:text-black
+          active:bg-yellow-500 focus:bg-red-500
          "
         >
           Checkout
-        </div>
+        </button>
       </div>
       <div className="bg-white overflow-hidden rounded-3xl shadow-xl">
         <div className="bg-blue-500 p-6 pb-14">
@@ -60,10 +62,10 @@ const Home: NextPage = () => {
           <span className="font-medium text-xl">Swoon Lounge</span>
           <span className="text-xs text-gray-500">Chair</span>
           <div className="mt-3 mb-5 flex justify-between items-center">
-            <div>
-              <input type="radio" />
-              <input type="radio" />
-              <input type="radio" />
+            <div className="space-x-2">
+              <button className="w-5 h-5 rounded-full bg-yellow-500" />
+              <button className="w-5 h-5 rounded-full bg-indigo-500" />
+              <button className="w-5 h-5 rounded-full bg-teal-500" />
             </div>
             <div className="flex items-center space-x-5">
               <button className=" rounded-lg bg-blue-200 flex justify-center items-center aspect-square w-8 text-xl text-gray-500">
