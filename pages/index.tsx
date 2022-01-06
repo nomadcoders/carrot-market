7243e4bb732d6b35f7906a9ffd1e8ae82b61f60d
Nicolás Serrano Arévalo <itnico.las.me@gmail.com>
2022-01-06 16:38:21 +0900

    2.1 Test Drive part One

--- pages/index.tsx
+++ pages/index.tsx
@@ -25,9 +25,9 @@ const Home: NextPage = () => {
           Checkout
         </div>
       </div>
-      <div className="bg-white p-10 rounded-2xl shadow-xl"></div>
-      <div className="bg-white p-10 rounded-2xl shadow-xl"></div>
-      <div className="bg-white p-10 rounded-2xl shadow-xl"></div>
+      <div className="bg-white p-6 rounded-3xl shadow-xl"></div>
+      <div className="bg-white p-6 rounded-3xl shadow-xl"></div>
+      <div className="bg-white p-6 rounded-3xl shadow-xl"></div>
     </div>
   );
 };
