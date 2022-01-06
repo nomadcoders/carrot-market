6f6942df39e36be8e369e4d5b7e7aa54c6d89e4e
Nicolás Serrano Arévalo <itnico.las.me@gmail.com>
2022-01-06 16:53:38 +0900

    2.2 Test Drive part Two

--- pages/index.tsx
+++ pages/index.tsx
@@ -2,7 +2,7 @@ import type { NextPage } from "next";
 
 const Home: NextPage = () => {
   return (
-    <div className="bg-slate-400 py-20 px-10 grid gap-10">
+    <div className="bg-slate-400 py-20 px-20 grid gap-10">
       <div className="bg-white p-6 rounded-3xl shadow-xl">
         <span className="font-semibold text-3xl">Select Item</span>
         <div className="flex justify-between my-2">
@@ -25,7 +25,28 @@ const Home: NextPage = () => {
           Checkout
         </div>
       </div>
-      <div className="bg-white p-6 rounded-3xl shadow-xl"></div>
+      <div className="bg-white overflow-hidden rounded-3xl shadow-xl">
+        <div className="bg-blue-500 p-6 pb-14">
+          <span className="text-white text-2xl">Profile</span>
+        </div>
+        <div className="rounded-3xl p-6 bg-white relative -top-5">
+          <div className="flex relative -top-16 items-end justify-between">
+            <div className="flex flex-col items-center">
+              <span className="text-sm text-gray-500">Orders</span>
+              <span className="font-medium">340</span>
+            </div>
+            <div className="h-24 w-24 bg-red-400 rounded-full" />
+            <div className="flex flex-col items-center">
+              <span className="text-sm text-gray-500">Spent</span>
+              <span className="font-medium">$340</span>
+            </div>
+          </div>
+          <div className="relative  flex flex-col items-center -mt-10 -mb-5">
+            <span className="text-lg font-medium">Tony Molloy</span>
+            <span className="text-sm text-gray-500">미국</span>
+          </div>
+        </div>
+      </div>
       <div className="bg-white p-6 rounded-3xl shadow-xl"></div>
       <div className="bg-white p-6 rounded-3xl shadow-xl"></div>
     </div>
