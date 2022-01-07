8621d05261220d161efb5aea51d67a3c2d90182a
Nicolás Serrano Arévalo <itnico.las.me@gmail.com>
2022-01-07 12:58:42 +0900

    2.5 Transitions

--- pages/index.tsx
+++ pages/index.tsx
@@ -19,10 +19,10 @@ const Home: NextPage = () => {
         </div>
         <button
           className="mt-5 bg-blue-500 text-white p-3
-          text-center rounded-xl w-3/4 mx-auto 
+          text-center rounded-xl w-3/4 block mx-auto 
           hover:bg-teal-500 hover:text-black
           active:bg-yellow-500 focus:bg-red-500
-         "
+          "
         >
           Checkout
         </button>
@@ -63,9 +63,9 @@ const Home: NextPage = () => {
           <span className="text-xs text-gray-500">Chair</span>
           <div className="mt-3 mb-5 flex justify-between items-center">
             <div className="space-x-2">
-              <button className="w-5 h-5 rounded-full bg-yellow-500" />
-              <button className="w-5 h-5 rounded-full bg-indigo-500" />
-              <button className="w-5 h-5 rounded-full bg-teal-500" />
+              <button className="w-5 h-5 rounded-full bg-yellow-500 focus:ring-2 ring-offset-2 ring-yellow-500 transition" />
+              <button className="w-5 h-5 rounded-full bg-indigo-500 focus:ring-2 ring-offset-2 ring-indigo-500 transition" />
+              <button className="w-5 h-5 rounded-full bg-teal-500 focus:ring-2 ring-offset-2 ring-teal-500 transition" />
             </div>
             <div className="flex items-center space-x-5">
               <button className=" rounded-lg bg-blue-200 flex justify-center items-center aspect-square w-8 text-xl text-gray-500">
