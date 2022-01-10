cfad8451326c5acf629542a33b78876f8f5d051b
Nicolás Serrano Arévalo <itnico.las.me@gmail.com>
2022-01-10 23:56:52 +0900

    3.7 Write

---
+++ pages/community/write.tsx
@@ -0,0 +1,18 @@
+import type { NextPage } from "next";
+
+const Write: NextPage = () => {
+  return (
+    <form className="px-4 py-10">
+      <textarea
+        className="mt-1 shadow-sm w-full focus:ring-orange-500 rounded-md border-gray-300 focus:border-orange-500 "
+        rows={4}
+        placeholder="Ask a question!"
+      />
+      <button className="mt-2 w-full bg-orange-500 hover:bg-orange-600 text-white py-2 px-4 border border-transparent rounded-md shadow-sm text-sm font-medium focus:ring-2 focus:ring-offset-2 focus:ring-orange-500 focus:outline-none ">
+        Submit
+      </button>
+    </form>
+  );
+};
+
+export default Write;
