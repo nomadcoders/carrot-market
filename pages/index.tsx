9415766e39b1649b80ecf9cf354e83eee09a6a56
Nicolás Serrano Arévalo <itnico.las.me@gmail.com>
2022-01-10 20:41:28 +0900

    3.1 Auth part One

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,11 +1,7 @@
 import type { NextPage } from "next";
 
 const Home: NextPage = () => {
-  return (
-    <div className="dark:md:hover:bg-teal-400 bg-[url('/vercel.svg')]">
-      <h2 className="text-[97851px] text-[#000]">Hello</h2>
-    </div>
-  );
+  return null;
 };
 
 export default Home;
