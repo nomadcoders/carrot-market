2c173f4f3a3dce782c80c1aa85639d6bfd7bf878
Nicolás Serrano Arévalo <itnico.las.me@gmail.com>
2022-01-11 20:28:40 +0900

    3.9 Chat Detail

---
+++ pages/chats/[id].tsx
@@ -0,0 +1,30 @@
+import type { NextPage } from "next";
+
+const ChatDetail: NextPage = () => {
+  return (
+    <div>
+      <div>
+        <div />
+        <div>Hi how much are you selling them for?</div>
+      </div>
+      <div>
+        <div />
+        <div>I want ￦20,000</div>
+      </div>
+      <div>
+        <div />
+        <div>미쳤어</div>
+      </div>
+      <div>
+        <div>
+          <input type="text" />
+          <div>
+            <span>&rarr;</span>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default ChatDetail;
