1cbf3c5985465a738e4e0849c050a75e05104817
Nicolás Serrano Arévalo <itnico.las.me@gmail.com>
2022-01-11 21:52:52 +0900

    3.11 Bought, Loved and Sold

--- pages/profile/index.tsx
+++ pages/profile/index.tsx
@@ -3,16 +3,16 @@ import type { NextPage } from "next";
 const Profile: NextPage = () => {
   return (
     <div className="py-10 px-4">
-      <div>
-        <div />
-        <div>
-          <span>Steve Jebs</span>
-          <span>Edit profile &rarr;</span>
+      <div className="flex items-center space-x-3">
+        <div className="w-16 h-16 bg-slate-500 rounded-full" />
+        <div className="flex flex-col">
+          <span className="font-medium text-gray-900">Steve Jebs</span>
+          <span className="text-sm text-gray-700">Edit profile &rarr;</span>
         </div>
       </div>
-      <div>
-        <div>
-          <div>
+      <div className="mt-10 flex justify-around">
+        <div className="flex flex-col items-center">
+          <div className="w-14 h-14 text-white bg-orange-400 rounded-full flex items-center justify-center">
             <svg
               className="w-6 h-6"
               fill="none"
@@ -28,10 +28,12 @@ const Profile: NextPage = () => {
               ></path>
             </svg>
           </div>
-          <span>판매내역</span>
+          <span className="text-sm mt-2 font-medium text-gray-700">
+            판매내역
+          </span>
         </div>
-        <div>
-          <div>
+        <div className="flex flex-col items-center">
+          <div className="w-14 h-14 text-white bg-orange-400 rounded-full flex items-center justify-center">
             <svg
               className="w-6 h-6"
               fill="none"
@@ -47,10 +49,12 @@ const Profile: NextPage = () => {
               ></path>
             </svg>
           </div>
-          <span>구매내역</span>
+          <span className="text-sm mt-2 font-medium text-gray-700">
+            구매내역
+          </span>
         </div>
-        <div>
-          <div>
+        <div className="flex flex-col items-center">
+          <div className="w-14 h-14 text-white bg-orange-400 rounded-full flex items-center justify-center">
             <svg
               className="w-6 h-6"
               fill="none"
@@ -66,15 +70,17 @@ const Profile: NextPage = () => {
               ></path>
             </svg>
           </div>
-          <span>관심목록</span>
+          <span className="text-sm mt-2 font-medium text-gray-700">
+            관심목록
+          </span>
         </div>
       </div>
-      <div>
-        <div>
-          <div />
+      <div className="mt-12">
+        <div className="flex space-x-4 items-center">
+          <div className="w-12 h-12 rounded-full bg-slate-500" />
           <div>
-            <h4>니꼬</h4>
-            <div>
+            <h4 className="text-sm font-bold text-gray-800">니꼬</h4>
+            <div className="flex items-center">
               <svg
                 className="text-yellow-400 h-5 w-5"
                 xmlns="http://www.w3.org/2000/svg"
@@ -123,7 +129,7 @@ const Profile: NextPage = () => {
             </div>
           </div>
         </div>
-        <div>
+        <div className="mt-4 text-gray-600 text-sm">
           <p>
             Normally, both your asses would be dead as fucking fried chicken,
             but you happen to pull this shit while I&apos;m in a transitional
