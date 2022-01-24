4d41e1ce0d31cffd3fea27d60d13895a15e52a01
니꼬 <nico@nomadcoders.co>
2022-01-24 16:36:17 +0900

    5.5 Errors

--- pages/forms.tsx
+++ pages/forms.tsx
@@ -1,7 +1,6 @@
 import { FieldErrors, useForm } from "react-hook-form";
 
-// Better validation
-// Better Erros (set, clear, display)
+// Better Erros (set, clear)
 // Have control over inputs
 
 interface LoginForm {
@@ -11,7 +10,13 @@ interface LoginForm {
 }
 
 export default function Forms() {
-  const { register, handleSubmit } = useForm<LoginForm>();
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm<LoginForm>({
+    mode: "onChange",
+  });
   const onValid = (data: LoginForm) => {
     console.log("im valid bby");
   };
@@ -32,10 +37,17 @@ export default function Forms() {
         placeholder="Username"
       />
       <input
-        {...register("email", { required: "Email is required" })}
+        {...register("email", {
+          required: "Email is required",
+          validate: {
+            notGmail: (value) =>
+              !value.includes("@gmail.com") || "Gmail is not allowed",
+          },
+        })}
         type="email"
         placeholder="Email"
       />
+      {errors.email?.message}
       <input
         {...register("password", { required: "Password is required" })}
         type="password"
