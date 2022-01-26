d8006193eb44a79e96b1c2dd8d2a45640f77129c
니꼬 <nico@nomadcoders.co>
2022-01-26 15:31:19 +0900

    7.1 Accounts Logic

--- pages/api/users/enter.tsx
+++ pages/api/users/enter.tsx
@@ -3,7 +3,55 @@ import withHandler from "@libs/server/withHandler";
 import client from "@libs/server/client";
 
 async function handler(req: NextApiRequest, res: NextApiResponse) {
-  console.log(req.body);
+  const { phone, email } = req.body;
+  const payload = phone ? { phone: +phone } : { email };
+  const user = await client.user.upsert({
+    where: {
+      ...payload,
+    },
+    create: {
+      name: "Anonymous",
+      ...payload,
+    },
+    update: {},
+  });
+  console.log(user);
+  /* if (email) {
+    user = await client.user.findUnique({
+      where: {
+        email,
+      },
+    });
+    if (user) console.log("found it.");
+    if (!user) {
+      console.log("Did not find. Will create.");
+      user = await client.user.create({
+        data: {
+          name: "Anonymous",
+          email,
+        },
+      });
+    }
+    console.log(user);
+  }
+  if (phone) {
+    user = await client.user.findUnique({
+      where: {
+        phone: +phone,
+      },
+    });
+    if (user) console.log("found it.");
+    if (!user) {
+      console.log("Did not find. Will create.");
+      user = await client.user.create({
+        data: {
+          name: "Anonymous",
+          phone: +phone,
+        },
+      });
+    }
+    console.log(user);
+  } */
   return res.status(200).end();
 }
 
