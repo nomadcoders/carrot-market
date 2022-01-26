d4e5a84f0e089cc194dfb99b2ef8c7c44b1faac7
니꼬 <nico@nomadcoders.co>
2022-01-26 17:27:10 +0900

    7.4 Sending SMS

--- pages/api/users/enter.tsx
+++ pages/api/users/enter.tsx
@@ -1,6 +1,9 @@
+import twilio from "twilio";
 import { NextApiRequest, NextApiResponse } from "next";
 import withHandler, { ResponseType } from "@libs/server/withHandler";
 import client from "@libs/server/client";
+
+const twilioClient = twilio(process.env.TWILIO_SID, process.env.TWILIO_TOKEN);
 
 async function handler(
   req: NextApiRequest,
@@ -26,6 +29,14 @@ async function handler(
       },
     },
   });
+  if (phone) {
+    const message = await twilioClient.messages.create({
+      messagingServiceSid: process.env.TWILIO_MSID,
+      to: process.env.MY_PHONE!,
+      body: `Your login token is ${payload}.`,
+    });
+    console.log(message);
+  }
   return res.json({
     ok: true,
   });
