33a8968c88824c150ef1919ddf743f218069fca8
니꼬 <nico@nomadcoders.co>
2022-02-05 17:19:27 +0900

    11.1 Reviews

---
+++ pages/api/reviews.ts
@@ -0,0 +1,30 @@
+import { NextApiRequest, NextApiResponse } from "next";
+import withHandler, { ResponseType } from "@libs/server/withHandler";
+import client from "@libs/server/client";
+import { withApiSession } from "@libs/server/withSession";
+
+async function handler(
+  req: NextApiRequest,
+  res: NextApiResponse<ResponseType>
+) {
+  const {
+    session: { user },
+  } = req;
+  const reviews = await client.review.findMany({
+    where: {
+      createdForId: user?.id,
+    },
+    include: { createdBy: { select: { id: true, name: true, avatar: true } } },
+  });
+  res.json({
+    ok: true,
+    reviews,
+  });
+}
+
+export default withApiSession(
+  withHandler({
+    methods: ["GET"],
+    handler,
+  })
+);
