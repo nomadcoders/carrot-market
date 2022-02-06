449421ebfaedecd33d87d7f362212f3f24ae324c
니꼬 <nico@nomadcoders.co>
2022-02-06 23:33:28 +0900

    14.1 Local Images

--- pages/index.tsx
+++ pages/index.tsx
@@ -6,6 +6,8 @@ import useUser from "@libs/client/useUser";
 import Head from "next/head";
 import useSWR from "swr";
 import { Product } from "@prisma/client";
+import Image from "next/image";
+import riceCake from "../public/local.jpeg";
 
 export interface ProductWithCount extends Product {
   _count: {
@@ -54,6 +56,7 @@ const Home: NextPage = () => {
           </svg>
         </FloatingButton>
       </div>
+      <Image src={riceCake} placeholder="blur" quality={5} />
     </Layout>
   );
 };
