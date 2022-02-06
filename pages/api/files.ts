8bfb32372e72f6db9496f97e5ad9a9029766e556
니꼬 <nico@nomadcoders.co>
2022-02-06 19:10:45 +0900

    13.4 Direct Upload URL

--- pages/api/files.ts
+++ pages/api/files.ts
@@ -7,15 +7,28 @@ async function handler(
   req: NextApiRequest,
   res: NextApiResponse<ResponseType>
 ) {
+  const response = await (
+    await fetch(
+      `https://api.cloudflare.com/client/v4/accounts/${process.env.CF_ID}/images/v1/direct_upload`,
+      {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+          Authorization: `Bearer ${process.env.CF_TOKEN}`,
+        },
+      }
+    )
+  ).json();
+  console.log(response);
   res.json({
     ok: true,
-    url: "",
+    ...response.result,
   });
 }
 
 export default withApiSession(
   withHandler({
-    methods: ["POST"],
+    methods: ["GET"],
     handler,
   })
 );
--- pages/profile/edit.tsx
+++ pages/profile/edit.tsx
@@ -37,18 +37,31 @@ const EditProfile: NextPage = () => {
   }, [user, setValue]);
   const [editProfile, { data, loading }] =
     useMutation<EditProfileResponse>(`/api/users/me`);
-  const onValid = ({ email, phone, name, avatar }: EditProfileForm) => {
+  const onValid = async ({ email, phone, name, avatar }: EditProfileForm) => {
     if (loading) return;
     if (email === "" && phone === "" && name === "") {
       return setError("formErrors", {
         message: "Email OR Phone number are required. You need to choose one.",
       });
     }
-    editProfile({
-      email,
-      phone,
-      name,
-    });
+    if (avatar && avatar.length > 0) {
+      const cloudflareRequest = await (await fetch(`/api/files`)).json();
+      console.log(cloudflareRequest);
+      // upload file to CF URL
+      return;
+      editProfile({
+        email,
+        phone,
+        name,
+        // avatarUrl: CF URL
+      });
+    } else {
+      editProfile({
+        email,
+        phone,
+        name,
+      });
+    }
   };
   useEffect(() => {
     if (data && !data.ok && data.error) {
