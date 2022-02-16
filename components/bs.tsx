cfab2fd5605e8ae04c152b8a9f9d8ddce3577b4e
니꼬 <nico@nomadcoders.co>
2022-02-17 00:00:40 +0900

    17.2 Dynamic Imports

---
+++ components/bs.tsx
@@ -0,0 +1,4 @@
+console.log("hello im bs");
+export default function Bs() {
+  return <h1>hello</h1>;
+}
--- pages/enter.tsx
+++ pages/enter.tsx
@@ -6,6 +6,10 @@ import Input from "@components/input";
 import useMutation from "@libs/client/useMutation";
 import { cls } from "@libs/client/utils";
 import { useRouter } from "next/router";
+import dynamic from "next/dynamic";
+// import Bs from "@components/bs";
+
+const Bs = dynamic(() => import("@components/bs"), { ssr: false });
 
 interface EnterForm {
   email?: string;
@@ -118,14 +122,17 @@ const Enter: NextPage = () => {
                 />
               ) : null}
               {method === "phone" ? (
-                <Input
-                  register={register("phone")}
-                  name="phone"
-                  label="Phone number"
-                  type="number"
-                  kind="phone"
-                  required
-                />
+                <>
+                  <Bs />
+                  <Input
+                    register={register("phone")}
+                    name="phone"
+                    label="Phone number"
+                    type="number"
+                    kind="phone"
+                    required
+                  />
+                </>
               ) : null}
               {method === "email" ? (
                 <Button text={loading ? "Loading" : "Get login link"} />
