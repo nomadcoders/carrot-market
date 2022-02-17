71bfb64b56e4cd8d517c50d834fb063eb0ea1c7d
니꼬 <nico@nomadcoders.co>
2022-02-18 00:47:42 +0900

    17.12 getStaticPaths part Two

--- pages/blog/[slug].tsx
+++ pages/blog/[slug].tsx
@@ -1,9 +1,20 @@
-import { DMMF } from "@prisma/client/runtime";
+import { readdirSync } from "fs";
 import { NextPage } from "next";
 
 const Post: NextPage = () => {
   return <h1>hi</h1>;
 };
+
+export function getStaticPaths() {
+  const files = readdirSync("./posts").map((file) => {
+    const [name, extension] = file.split(".");
+    return { params: { slug: name } };
+  });
+  return {
+    paths: files,
+    fallback: false,
+  };
+}
 
 export function getStaticProps() {
   return {
