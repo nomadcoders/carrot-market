689b641f4fb664f0ecfda69d28b4640d8397f219
니꼬 <nico@nomadcoders.co>
2022-02-25 01:09:58 +0900

    19.4 Parallelism

--- pages/coins.server.tsx
+++ pages/coins.server.tsx
@@ -3,24 +3,46 @@ import { Suspense } from "react";
 const cache: any = {};
 function fetchData(url: string) {
   if (!cache[url]) {
-    throw fetch(url)
-      .then((r) => r.json())
-      .then((json) => (cache[url] = json.slice(0, 50)));
+    throw Promise.all([
+      fetch(url)
+        .then((r) => r.json())
+        .then((json) => (cache[url] = json)),
+      new Promise((resolve) =>
+        setTimeout(resolve, Math.round(Math.random() * 10555))
+      ),
+    ]);
   }
   return cache[url];
 }
 
+function Coin({ id, name, symbol }: any) {
+  const {
+    quotes: {
+      USD: { price },
+    },
+  } = fetchData(`https://api.coinpaprika.com/v1/tickers/${id}`);
+  return (
+    <span>
+      {name} / {symbol}: ${price}
+    </span>
+  );
+}
+
 function List() {
   const coins = fetchData("https://api.coinpaprika.com/v1/coins");
-  console.log(coins);
   return (
-    <ul>
-      {coins.map((coin: any) => (
-        <li key={coin.id}>
-          {coin.name} / {coin.symbol}
-        </li>
-      ))}
-    </ul>
+    <div>
+      <h4>List is done</h4>
+      <ul>
+        {coins.slice(0, 10).map((coin: any) => (
+          <li key={coin.id}>
+            <Suspense fallback={`Coin ${coin.name} is loading`}>
+              <Coin {...coin} />
+            </Suspense>
+          </li>
+        ))}
+      </ul>
+    </div>
   );
 }
 
