edaae92fa8137a10d827be7bdcef43965b482ca0
Nicolás Serrano Arévalo <itnico.las.me@gmail.com>
2022-05-23 12:48:33 -0500

    Safari Bugfix

--- libs/server/withSession.ts
+++ libs/server/withSession.ts
@@ -1,3 +1,4 @@
+import { IronSessionOptions } from "iron-session";
 import { withIronSessionApiRoute, withIronSessionSsr } from "iron-session/next";
 
 declare module "iron-session" {
@@ -8,15 +9,18 @@ declare module "iron-session" {
   }
 }
 
-const cookieOptions = {
+const cookieConfig: IronSessionOptions = {
   cookieName: "carrotsession",
   password: process.env.COOKIE_PASSWORD!,
+  cookieOptions: {
+    secure: process.env.NODE_ENV === "production",
+  },
 };
 
 export function withApiSession(fn: any) {
-  return withIronSessionApiRoute(fn, cookieOptions);
+  return withIronSessionApiRoute(fn, cookieConfig);
 }
 
 export function withSsrSession(handler: any) {
-  return withIronSessionSsr(handler, cookieOptions);
+  return withIronSessionSsr(handler, cookieConfig);
 }
